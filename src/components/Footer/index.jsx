8ecb0ea098eb28b0873e3ddedbd0d68d4be0a605
Cesar-Andres-Puma <cesar93puma@gmail.com>
2022-06-03 19:43:25 -0300

    resolvendo erro do git

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,21 @@
+import * as S from "./style"
+import LogoZap from "../../public/assets/logo-zap.svg"
+import LogoInst from "../../public/assets/logo-instragram.svg"
+import LogoLink from "../../public/assets/logo-linkedin.svg"
+import LogoGit from "../../public/assets/logo-git-hub.svg"
 export const Rodapé = () => {
-  return <h1>esse é o rodapé</h1>;
+  return (
+    <>
+    <S.Footer>
+      <S.Titulo>Design e desenvolvimento feito por <span> Lucas Biasi.</span></S.Titulo>
+      <S.Lista>
+        <S.ItemDaLista><img src={LogoZap} alt="" /></S.ItemDaLista>
+        <li><img src={LogoInst} alt="" /></li>
+        <li><img src={LogoLink} alt="" /></li>
+        <li><img src={LogoGit} alt="" /></li>
+      </S.Lista>
+    </S.Footer>
+    </>
+  )
+   
 };
---
+++ src/components/Footer/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+import { themes } from "../../../styles/themes"
+export const Footer = styled.footer`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2.5rem 5rem;
+    background-color:${themes.corlaranja};
+  
+`
+export const Titulo = styled.h1`
+color:${themes.branco};
+`
+export const Lista = styled.ul`
+display: flex;
+`
+export const ItemDaLista = styled.li`
+padding: 1rem;
+`
+
